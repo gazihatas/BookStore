691d821b895c09d07cd838b5f9ff99bb8a018ab0
Gazi Hataş <gazihatas71@gmail.com>
2022-09-18 21:17:12 +0300

    Delete endpoininin yazılması

--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -93,6 +93,19 @@ namespace WebApi.AddControllers{
             return Ok();
         }
 
+        //Delete
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            var book =  BookList.SingleOrDefault(x => x.Id == id);
+            if (book is null)
+            {
+                return BadRequest();
+            }
+
+            BookList.Remove(book);
+            return Ok();
+        }
 
     }
 }

fd7bbb7069e7fde56aaaea5a6799227f5eb245fb
Gazi Hataş <gazihatas71@gmail.com>
2022-09-20 17:56:26 +0300

    Model Doğrulama | Fluent Validation | CreateBook ve DeleteBook validatorleri yazıldı

--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using AutoMapper;
+using FluentValidation;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.BookOperations.CreateBook;
 using WebApi.BookOperations.DeleteBook;
@@ -72,7 +74,22 @@ namespace WebApi.AddControllers{
             try
             {
                 command.Model = newBook; 
+                CreateBookCommandValidator validator = new CreateBookCommandValidator();
+                validator.ValidateAndThrow(command);
                 command.Handle();
+
+                //bütün kurallardan geçmediyse
+                // if(!result.IsValid)
+                // {
+                //     foreach(var item in result.Errors)
+                //     {
+                //         Console.WriteLine("Özellik " + item.PropertyName + "- Error Message: " + item.ErrorMessage);
+                //     }
+                // }
+                // else
+                // {
+                //     command.Handle();
+                // }
             }
             catch (Exception ex)
             {
@@ -110,6 +127,8 @@ namespace WebApi.AddControllers{
             {
                 DeleteBookCommand command = new DeleteBookCommand(_context);
                 command.BookId = id;
+                DeleteBookCommandValidator validator = new DeleteBookCommandValidator();
+                validator.ValidateAndThrow(command);
                 command.Handle();
             }
             catch (Exception ex)
